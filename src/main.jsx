269e0dc257c3d8f7126573a8d4958e0e48506f35
Omar Mahdy <72965518+mahdtomar@users.noreply.github.com>
2025-01-08 13:40:29 +0200

    updating for vercel deployment 01

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/full-stack-e-commerce" future={{
+  <BrowserRouter basename="/" future={{
     v7_startTransition: true,
   }}>
     <App />
